8edde23b78141c9aabaed2b15917219c16bb04d1
Jaspreet Singh <mail@jas.bio>
2022-09-24 22:36:02 +0400

    Update dato.rb

--- Casks/dato.rb
+++ Casks/dato.rb
@@ -2,7 +2,7 @@ cask "dato" do
   version "4.1.0"
   sha256 "bd969e806819edd5ef1148a6e8ebf6fa52abc24e9becfedbab3fa153f3ed6f36"
 
-  url "https://dmg.s3.nl-ams.scw.cloud/Dato_#{version}_MAS_TNT.dmg"
+  url "https://dmg.s3.nl-ams.scw.cloud/Dato_#{version}.dmg"
   name "Dato"
   desc ""
   homepage ""

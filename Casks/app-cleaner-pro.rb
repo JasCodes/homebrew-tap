9e48d844d34269a7b72b0945034cb17b7ed8e0df
Jaspreet Singh <mail@jas.bio>
2022-09-30 22:34:33 +0400

    fix typo

--- Casks/app-cleaner-pro.rb
+++ Casks/app-cleaner-pro.rb
@@ -3,7 +3,7 @@ cask "app-cleaner-pro" do
   sha256 "f0cc24ea3c173d7cc7f3d9efb6ec8114daf25d7434c34e81985cca36b7d4ab90"
 
   url "https://dmg.s3.nl-ams.scw.cloud/App_Cleaner_PRO_#{version}.dmg"
-  name "app-clearner-pro"
+  name "app-cleaner-pro"
   desc ""
   homepage ""
 

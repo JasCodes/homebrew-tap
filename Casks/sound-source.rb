74aed3799a876993550c6be5265e542f70d9e519
Jaspreet Singh <mail@jas.bio>
2022-09-30 22:05:44 +0400

    add sound source

---
+++ Casks/sound-source.rb
@@ -0,0 +1,11 @@
+cask "sound-source" do
+  version "5.5.4"
+  sha256 "bc4a37657fcb9583efc798d3e7cc7a47f59f3d95f540fad4d8aa36091f490333"
+
+  url "https://dmg.s3.nl-ams.scw.cloud/SoundSource_#{version}.dmg"
+  name "sound-source"
+  desc ""
+  homepage ""
+
+  app ""
+end

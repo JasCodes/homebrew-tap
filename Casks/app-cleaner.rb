2380482223e47f4ccd556103ed8201f181a48042
Jaspreet Singh <mail@jas.bio>
2022-10-27 23:14:32 +0400

    Add bartender

--- Casks/app-cleaner.rb
+++ Casks/app-cleaner.rb
@@ -1,9 +1,9 @@
-cask "app-cleaner-pro" do
+cask "app-cleaner" do
   version "7.8.2"
   sha256 "f0cc24ea3c173d7cc7f3d9efb6ec8114daf25d7434c34e81985cca36b7d4ab90"
 
   url "https://dmg.s3.nl-ams.scw.cloud/App_Cleaner_PRO_#{version}.dmg"
-  name "app-cleaner-pro"
+  name "app-cleaner"
   desc ""
   homepage ""
 

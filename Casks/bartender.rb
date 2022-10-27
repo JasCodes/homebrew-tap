b5d83eb630addcd125c8b46185811e54f2a84930
Jaspreet Singh <mail@jas.bio>
2022-10-27 23:23:22 +0400

    fix bartender

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -1,5 +1,5 @@
 cask "bartender" do
-  version "4.4.2.11"
+  version "4.2.11"
   sha256 "c2252a2613e15f0c0d6f4e129fb5beb841165b0148d2a0490b948c50b06033b2"
 
   url "https://dmg.s3.nl-ams.scw.cloud/Bartender_#{version}.dmg"
@@ -7,5 +7,5 @@ cask "bartender" do
   desc ""
   homepage ""
 
-  app ""
+  app "Bartender 4.app"
 end

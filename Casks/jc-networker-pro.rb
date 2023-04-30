97df6ea62b249a55407e1dffb3a5f58223df2f79
Jaspreet Singh <jaspreet@glulearning.com>
2023-04-30 09:20:06 +0400

    add networker pro

---
+++ Casks/jc-networker-pro.rb
@@ -0,0 +1,11 @@
+cask "jc-networker-pro" do
+  version "8.5.6"
+  sha256 "ea09e984b000872a76ec5733b9cdfe6a5958c6e7add183cccc3213525e534b84"
+  
+  url "https://dmg.s3.nl-ams.scw.cloud/NetWorker_Pro_#{version}.dmg"
+  name "jc-networker-pro"
+  desc ""
+  homepage ""
+
+  app "Networker Pro.app"
+end
